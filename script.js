2ca70009805d0d8ad57b2f3343ab5fe3e4d8fe33
FaizSangatAmadGntnk <faizidiandaru@gmail.com>
2025-04-18 14:33:51 +0700

    update komunitas epep

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navLinks = document.getElementById('nav-links');
 navToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+// Scroll navbar transparent
+window.addEventListener('scroll', function () {
+    const navbar = this.document.querySelector('.navbar');
+    if (this.window.scrollY > 10) {
+        navbar.classList.add('scrolled');
+    }else{
+        navbar.classList.remove('scrolled');
+    }
+})
